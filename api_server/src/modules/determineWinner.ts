ab8f77a89976e1c7ffa36cd8c570594f78a8e5bf
Trainee (Mohammed Abdulla Mohammed Hassan Alhosani) <MAB52339@emiratesnbd.com>
2023-02-07 11:37:23 +0400

    Added comments and cleaned up code

--- api_server/src/modules/determineWinner.ts
+++ api_server/src/modules/determineWinner.ts
@@ -1,29 +1,31 @@
 import throwResult from "../types/throwResult";
 
 export const determineWinner = (results: throwResult[]) => {
+    // Set to store players in case of a tie
     let tiedPlayers = new Set<number>();
+
+    // Stores the highest number value and the player who has that value
     let winnerPlayer = 0;
     let winnerScore = 0;
 
     results.forEach((result) => {
+        // If new maximum value is found, clear set and update variables
         if(result.score > winnerScore){
             tiedPlayers.clear();
             winnerPlayer = result.player;
             winnerScore = result.score;
         }
+        // if multiple players have the highest value, add them to the set
         else if(result.score === winnerScore){
             tiedPlayers.add(winnerPlayer);
             tiedPlayers.add(result.player);
         }
     });
+
+    // If the set is empty, we have one winner
     if(tiedPlayers.size === 0){
         return {result: 'win', winner: winnerPlayer, score: winnerScore};
     }
-    else {
-        let tiedString = '';
-        tiedPlayers.forEach((player) => {
-            tiedString += `${player}, `;
-        })
-        return {result: 'tie', tiedPlayers: Array.from(tiedPlayers), score: winnerScore};
-    }
+    // Else we have a tie, and we output all tied players instead
+    return {result: 'tie', tiedPlayers: Array.from(tiedPlayers), score: winnerScore};
 }
--- api_server/src/modules/playerResult.ts
+++ api_server/src/modules/playerResult.ts
@@ -2,6 +2,7 @@ import throwResult from "../types/throwResult";
 
 export const playerResult = (playerNo: number, dice: number, throws: number): throwResult => {
     let score = 0;
+    // we throw each dice "throws" times, and add the result to score
     for(let j = 0; j < dice; j++) {
         for(let k = 0; k < throws; k++){
             score += Math.floor((Math.random() * 6) + 1);
