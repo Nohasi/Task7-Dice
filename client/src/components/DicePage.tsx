526c3d83210d3b28eb42d7137c0bee15757b5eae
Trainee (Mohammed Abdulla Mohammed Hassan Alhosani) <MAB52339@emiratesnbd.com>
2023-02-07 11:52:19 +0400

    Began setting up frontend based on previous work

---
+++ client/src/components/DicePage.tsx
@@ -0,0 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import { CoinflipForm } from './CoinflipForm';
+import { FlipsPanel } from './FlipsPanel';
+import { Header } from './Header';
+import { ResultPanel } from './ResultPanel';
+
+export const CoinflipPage = () => {
+
+    // TODO: Set States
+
+    return(
+        <div className="App">
+            <Header/>
+            <div className="container mrgnbtm">
+                <div className="row">
+                    <div className="col-md-8">
+                        {/* TODO: FORM */}
+                    </div>
+                    <div className="col-md-4">
+                        {/* TODO: RESULT PANEL */}
+                    </div>
+                </div>
+            </div>
+            <div className="container mrgnbtm">
+                {/* TODO: TABLE */}
+            </div>
+        </div>
+    );   
+}
