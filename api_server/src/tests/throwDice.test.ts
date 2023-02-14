2de3bcd403e93329a76dd3cf7aa12dda763fed65
Trainee (Mohammed Abdulla Mohammed Hassan Alhosani) <MAB52339@emiratesnbd.com>
2023-02-14 11:10:46 +0400

    wrote tests for the throwDice function

--- api_server/src/tests/throwDice.test.ts
+++ api_server/src/tests/throwDice.test.ts
@@ -1,5 +1,27 @@
 import { throwDice } from "../modules/throwDice";
 
 describe('Passing values correctly', () => {
-    it('Correctly returns values')
+    it('Correctly returns values for all players if everyone rolls a 6 every time', () => {
+        jest.spyOn(global.Math, 'random').mockReturnValue(0.95);
+        const players = 3;
+        const throws = 10;
+        const dice = 5;
+        
+        const result = throwDice(players, throws, dice);
+        expect(result[0].score).toEqual(300);
+        expect(result[1].score).toEqual(300);
+        expect(result[2].score).toEqual(300);
+    });
+
+    it('Correctly returns values for all players if everyone rolls a 1 every time', () => {
+        jest.spyOn(global.Math, 'random').mockReturnValue(0);
+        const players = 3;
+        const throws = 10;
+        const dice = 5;
+        
+        const result = throwDice(players, throws, dice);
+        expect(result[0].score).toEqual(50);
+        expect(result[1].score).toEqual(50);
+        expect(result[2].score).toEqual(50);
+    });
 });
