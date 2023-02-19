dd3cea11afe9527e6b9f3e297a7ae229836ce7f7
Trainee (Mohammed Abdulla Mohammed Hassan Alhosani) <MAB52339@emiratesnbd.com>
2023-02-19 19:39:25 +0400

    Made error display a more relevant color

--- client/src/components/ErrorResult.tsx
+++ client/src/components/ErrorResult.tsx
@@ -1,4 +1,7 @@
-export const ErrorResult = () => {
+export const ErrorResult = ({setColor}: {setColor:React.Dispatch<React.SetStateAction<string>>}) => {
+
+    setColor("#ee6b6e");
+
     return(
         <div>
             <div className="result">
--- client/src/tests/DiceForm.test.ts
+++
@@ -1,1 +0,0 @@
-import { DiceForm } from "../components/DiceForm";
